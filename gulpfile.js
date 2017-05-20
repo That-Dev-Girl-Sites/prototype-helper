6f34dd84fd77441f3b259a0fa6f6c3b5b10ea3f8
Joni Halabi <her@thatdevgirl.com>
2017-05-19 22:30:43 -0400

    Committing long lost changes

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp       = require('gulp');
+var sass       = require('gulp-sass');
+var minifyCSS  = require('gulp-minify-css');
+var watch      = require('gulp-watch');
+
+gulp.task('default', ['watch', 'build-css']);
+
+gulp.task('watch', function() {
+	gulp.watch('sass/*.scss', ['build-css']);
+});
+
+gulp.task('build-css', function() {
+	return gulp.src('sass/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(minifyCSS())
+		.pipe(gulp.dest('css'));
+});
