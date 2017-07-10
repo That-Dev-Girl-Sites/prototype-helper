a436c5e002dec5139ee75df6e3a50b2440ed77d9
Joni Halabi <her@thatdevgirl.com>
2017-07-10 13:47:58 -0400

    Actually getting this to work; added a bunch of sites

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,11 @@ var watch      = require('gulp-watch');
 gulp.task('default', ['watch', 'build-css']);
 
 gulp.task('watch', function() {
-	gulp.watch('sass/*.scss', ['build-css']);
+	gulp.watch('sass/**/*.scss', ['build-css']);
 });
 
 gulp.task('build-css', function() {
-	return gulp.src('sass/*.scss')
+	return gulp.src('sass/styles.scss')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(minifyCSS())
 		.pipe(gulp.dest('css'));
